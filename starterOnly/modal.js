bb75f845c683d86aff35abd5cfe144cf6b335b28
Laetitia Hars <laetitia.hars@gmail.com>
2021-02-18 23:05:37 +0100

    :bug: fix display menu-burger

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,11 +1,28 @@
+// NAVBAR
+
+// Open and close nav-responsive
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  let topNav = document.getElementById("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    topNav.className = "topnav";
   }
 }
+
+// Detect if click outside nav-bar if nav-bar responsive is open
+document.addEventListener("click", (evt) => {
+  const navResponsive = document.getElementById("main-navbar");
+  const targetElement = evt.target; // clicked element
+  const burgerMenu = document.getElementsByClassName("fa fa-bars")[0];
+  if (
+    targetElement !== navResponsive &&
+    targetElement !== burgerMenu &&
+    targetElement.offsetParent !== navResponsive
+  ) {
+    document.getElementById("myTopnav").className = "topnav";
+  }
+});
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -19,5 +36,3 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
