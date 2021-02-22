60db2bef27c9dff1c6e1964772505e785994d2f4
Laetitia Hars <laetitia.hars@gmail.com>
2021-02-22 15:35:56 +0100

    :sparkles: add close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,12 +27,22 @@ document.addEventListener("click", (evt) => {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
