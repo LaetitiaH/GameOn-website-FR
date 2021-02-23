3d63b2afe7f47babd104cc2646a486480e38dc04
Laetitia Hars <laetitia.hars@gmail.com>
2021-02-23 15:30:06 +0100

    ✨ add form control after focus out

--- src/modal.js
+++ src/modal.js
@@ -26,6 +26,7 @@ const modalBackground = document.querySelector(".bground");
 const modalButton = document.querySelectorAll(".btn-signup");
 const closeButton = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const input = document.querySelectorAll("input");
 
 // launch modal event
 modalButton.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -45,3 +46,27 @@ function closeModal() {
   modalBackground.style.display = "none";
   page.style.overflow = "auto";
 }
+
+// Check if formData is valid after focusout
+
+formData.forEach((formData) =>
+  formData.addEventListener("change", (event) => {
+    if (event.target.validity.tooShort) {
+      const label = formData.children[0].textContent.toLowerCase();
+      formData.setAttribute(
+        "data-error",
+        "Veuillez entrer 2 caractères ou plus pour le champ " + label
+      );
+      formData.setAttribute("data-error-visible", true);
+    } else if (!event.target.validity.valid) {
+      const label = formData.children[0].textContent.toLowerCase();
+      formData.setAttribute(
+        "data-error",
+        "Le champ " + label + " n'est pas correct"
+      );
+    } else {
+      formData.removeAttribute("data-error");
+      formData.setAttribute("data-error-visible", false);
+    }
+  })
+);
